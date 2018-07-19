94de76dd00004c333e95e1cba672f3bf8988bdcd
Qingfei Pan <33663247+QingfeiPan@users.noreply.github.com>
2018-07-18 21:15:49 -0500

    Add files via upload

---
+++ 02_scMINER/01_scMINER_input/03_run_scMINER_Input.sh
@@ -0,0 +1,2 @@
+Rscript -e "rmarkdown::render('/Volumes/yu3grp/scRNASeq/yu3grp/metastasis/02_Breast_cancer/02_scMINER/Combined_Tumor/Combined_Tumor_mm10_scMINER_input.Rmd')"
+Rscript -e "rmarkdown::render('/Volumes/yu3grp/scRNASeq/yu3grp/metastasis/02_Breast_cancer/02_scMINER/Combined_Immune/Combined_Immune_mm10_scMINER_input.Rmd')"
